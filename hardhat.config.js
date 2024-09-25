6a5b75b4ae7ccca26997cff87284699814dfb807
Ada Li <ada_li_li@yahoo.com>
2024-09-25 05:26:48 +0000

    Init

---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.27"
+      },
+      {
+        version: "0.8.20"
+      },
+      {
+        version: "0.7.6"
+      }
+    ]
+  }
+};
